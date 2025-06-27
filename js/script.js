e085678003e9a2d228e1453e0522317bc64fdc33
Juan421P <20210297@ricaldone.edu.sv>
2025-06-27 00:12:25 -0600

    Algo se ha hecho pero aún falta mucho

---
+++ js/script.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'tgm-gray-1': '#767676',
+                'tgm-gray-2': '#A5A5A5'
+            }
+        }
+    }
+}
